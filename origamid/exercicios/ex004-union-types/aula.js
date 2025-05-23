4abac9bee0856eb23a5eaa10d183217951af0aaa
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-23 15:16:10 +0200

    ex004-union-types

---
+++ origamid/exercicios/ex004-union-types/aula.js
@@ -0,0 +1,45 @@
+"use strict";
+// - Union Types
+// É comum termos funções que podem retornar ou receber tipos diferentes. Para isso usamos a barra vertical string | number | boolean.
+const total2 = 300;
+// - Funções
+// Funções podem receber parâmetros com diferentes tipos e também podem retornar diferentes tipos de dados.
+function isNumber(value) {
+    if (typeof value === "number") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isNumber(total2));
+//obs: se nao tivesse o else, ao colocar o mouse em cima da function ela diria que retornaria true ou undefined
+// - DOM
+// Funções que selecionam elementos do DOM geralmente retornam null como uma possibilidade de tipo, pois o TypeScript não tem acesso prévio ao DOM para saber se o elemento existe ou não.
+const btn = document.querySelector("button"); //const btn: HTMLButtonElement | null
+btn.click(); //'btn' é possivelmente 'null'.ts(18047)
+if (btn) {
+    //verifica se btn existe, se sim, passa como true e não tem mais como ser null
+    btn.click(); //const btn: HTMLButtonElement
+}
+//outra forma de fazer essa verificação seria com o Optional chaining:
+// ? - é dizer: continue acessando as propriedades (o que vem depois do ponto) caso o valor seja diferente de null ou undefined
+btn?.click(); //const btn: HTMLButtonElement | null
+//----------------Exercício
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+function toNumber(value) {
+    if (typeof value === "number") {
+        return value;
+    }
+    else if (typeof value === "string") {
+        return Number(value); //(value?: any) => number
+    }
+    else {
+        throw new Error("value deve ser um número ou uma string");
+    }
+}
+console.log(toNumber("20"));
