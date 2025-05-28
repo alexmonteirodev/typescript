dcac30630c426cb43c4162f8e9c635c3ad7ac47e
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-28 10:23:58 +0200

    ex017-functions

---
+++ origamid/exercicios/ex017-functions/aula.ts
@@ -0,0 +1,17 @@
+// - Functions
+// A interface de uma função é definida durante a sua declaração.
+
+function x(params: number): number {
+  return params * 2;
+}
+
+// - Void
+// No JavaScript, uma função sem return irá retornar sempre undefined. No TypeScript o retorno é definido como void. Isso evita usos errados como checagens booleanas de métodos que não possuem retorno.
+
+function pintarTela(cor: string) {
+  document.body.style.background = cor;
+}
+pintarTela("black"); //pintarTela(cor: string): void
+console.log(pintarTela("black"));
+
+console.log("teste");
